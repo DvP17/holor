7218ab83b87f8a2960c17ce8fb2850ff89eb97dd
DvP17 <dvp17@protonmail.ch>
2022-02-06 17:24:06 +0100

    updated quilt fun, it is now possible to have function inside

--- R/quilt.R
+++ R/quilt.R
@@ -4,7 +4,7 @@
 #' @param theta Is theta
 #' @param phi Is phi
 #' @param device Is Looking Glass Device
-quilt <- function(cone = 10, theta = 0, phi = 0, device = "LGP", file) {
+quilt <- function(fun, cone = 10, theta = 0, phi = 0, device = "LGP", file) {
 
   # Set perspectives
   str <- theta - cone / 2
@@ -29,4 +29,45 @@ quilt <- function(cone = 10, theta = 0, phi = 0, device = "LGP", file) {
   }
   dev.off()
 
+}
+
+
+# Testing
+
+# volc <- reshape2::melt(volcano)
+# with(volc, plot3D::scatter3D(x = Var1, y = Var2, z = value, ticktype="detailed", pch=16,
+#                      xlab="longitude", ylab="latitude", zlab="depth, km", main=""))
+
+quilt <- function(fun, cone = 10, theta = 0, phi = 0, device = "LGP", file) {
+
+  # Set perspectives
+  str <- theta - cone / 2
+  end <- theta + cone / 2
+  fov <- seq(str, end, (abs(str) + abs (end)) / (48-1)) # 48 is number of frames
+
+  # Set number of quilts
+  nrw <- 6 #row
+  ncl <- 8 #col
+  ord <- as.vector(sapply(rev(1:nrw*ncl - (ncl-1)), function(i) i:(i+(ncl-1))))
+
+  # LGP resolution
+  wid <- 1536*ncl
+  hei <- 2048*nrw
+
+  # Image naming
+  loc <- gsub("(\\.)([A-z]+)$", paste0("_qs", ncl, "x", nrw, "\\1\\2"), file)
+
+  # Call function
+  f_nam <- deparse(substitute(fun)[[1]]) # deparse for check
+  args <- substitute(fun)
+  args[[1]] <- NULL # delete first element (fun name)
+  if (grepl("scatter3D", f_nam)) {
+    require("plot3D")
+    png(loc, width = wid, height = hei)
+    par(mfrow = c(nrw, ncl))
+    for (i in fov[ord]) {
+      do.call("scatter3D", c(arg, list(theta = i, phi = phi, cex=20, pch=20)))
+    }
+    dev.off()
+  }
 }
--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 ![GitHub](https://img.shields.io/github/r-package/v/DvP17/holor) ![GitHub](https://img.shields.io/github/license/DvP17/holor)
 
 # holor Package
+
+A package that creates quilts for holographic images on the Looking Glass Device.
+
+## Dependencies
+
+As of right now the `holor` only works with the `plot3D` package.
+
+
+## Install
+
+```{r}
+devtools::install_github("DvP17/holor")
+```
+
+## Create a quilt
+
+```{r}
+library(holor)
+quilt(plot3D::scatter3D(x = iris$Sepal.Length,
+                        y = iris$Petal.Length,
+                        z = iris$Sepal.Width),
+      cone = 10, theta = 0, phi = 0, device = "LGP", file = "myquilt.png")
+```
+
+The `device` argument corresponds to the device you are using to display the hologram. The file name is automatically changed to include information on the parameters needed for HoloPlayStudio.
+
+Now you can export the quilt to HoloPlayStudio with drag & drop.
+
+
